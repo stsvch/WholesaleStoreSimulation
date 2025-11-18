607333c17423fc371e5d2fe15a75fe13c44ab6f3
stsvch <117557932+stsvch@users.noreply.github.com>
2025-11-18 14:18:53 +0300

    Fix chi-squared model build errors and document usage

--- WholesaleStoreSimulation/ChiSquaredTest.cs
+++ WholesaleStoreSimulation/ChiSquaredTest.cs
@@ -1,65 +1,9 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace WholesaleStoreSimulation
 {
-    /// <summary>
-    /// Содержит результаты выполнения теста хи-квадрат.
-    /// </summary>
-    public class ChiSquaredResult
-    {
-        public double ChiSquaredStatistic { get; set; }
-        public double CriticalValue { get; set; }
-        public int DegreesOfFreedom { get; set; }
-        public bool IsNormal { get; set; }
-        public List<IntervalInfo> Intervals { get; set; } = new();
-        public double Mean { get; set; }
-        public double StdDev { get; set; }
-    }
-
-    /// <summary>
-    /// Результат теста хи-квадрат для конкретного отклика.
-    /// </summary>
-    public class ChiSquaredMetricResult : ChiSquaredResult
-    {
-        public string MetricName { get; set; } = string.Empty;
-
-        public ChiSquaredMetricResult()
-        {
-        }
-
-        public ChiSquaredMetricResult(string metricName, ChiSquaredResult baseResult)
-        {
-            MetricName = metricName;
-            ChiSquaredStatistic = baseResult.ChiSquaredStatistic;
-            CriticalValue = baseResult.CriticalValue;
-            DegreesOfFreedom = baseResult.DegreesOfFreedom;
-            IsNormal = baseResult.IsNormal;
-            Mean = baseResult.Mean;
-            StdDev = baseResult.StdDev;
-            Intervals = baseResult.Intervals
-                .Select(i => new IntervalInfo
-                {
-                    LowerBound = i.LowerBound,
-                    UpperBound = i.UpperBound,
-                    ObservedFrequency = i.ObservedFrequency,
-                    ExpectedFrequency = i.ExpectedFrequency
-                })
-                .ToList();
-        }
-    }
-
-    /// <summary>
-    /// Хранит информацию об одном интервале гистограммы.
-    /// </summary>
-    public class IntervalInfo
-    {
-        public double LowerBound { get; set; }
-        public double UpperBound { get; set; }
-        public int ObservedFrequency { get; set; }
-        public double ExpectedFrequency { get; set; }
-    }
-    
     /// <summary>
     /// Реализует тест согласия хи-квадрат Пирсона для проверки гипотезы о нормальном распределении.
     /// </summary>
